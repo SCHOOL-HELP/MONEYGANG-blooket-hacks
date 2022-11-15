066b3bd88a6728110c459a9a2da14d887f8ccc20
NSB Gamer <116772462+NathanBollard@users.noreply.github.com>
2022-11-15 10:49:27 -0800

    Update and rename Fishing Frenzy/Set Lure.js to Fishing-Frenzy/Set-Lure-Rank.js

--- Fishing-Frenzy/Set-Lure-Rank.js
+++ Fishing-Frenzy/Set-Lure-Rank.js
@@ -6,8 +6,8 @@ window.prompt = iframe.contentWindow.prompt;
 
 var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner.stateNode;
 
-var lure = parseInt(prompt('Lure value? (1 to 5)')) - 1;
+var lure = parseInt(prompt('Lure rank value? (1 to 5)')) - 1;
 hack.setState({
     lure: lure
 });
-alert(`Lure set to ${lure + 1}!`);
+alert(`Lure rank set to ${lure + 1}!`);
